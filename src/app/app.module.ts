339cc8057190b0414a97f2ebd3c0e25dbf1839a6
eunisae sea <eunisae sea>
2017-12-07 00:08:27 +0100

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './Components/app.component';
+import { NavComponent } from './Components/Front/nav/nav.component';
+import { SliderComponent } from './Components/Front/slider/slider.component';
+import { FooterComponent } from './Components/Front/footer/footer.component';
+import { LoginComponent } from './Components/Front/login/login.component';
+import { FrontComponent } from './Components/Front/front/front.component';
+import { SignupComponent } from './Components/Front/signup/signup.component';
+
+const appRoutes: Routes = [
+  { path: '', component: FrontComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SliderComponent,
+    FooterComponent,
+    LoginComponent,
+    FrontComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+     appRoutes,
+     { enableTracing: true } // <-- debugging purposes only
+   )
   ],
   providers: [],
   bootstrap: [AppComponent]
